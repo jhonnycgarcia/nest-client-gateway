97fe60030ff725bb39b29b5d09351bd4f77a0bc3
Jhonny <jhonnycgarcia@gmail.com>
2025-06-05 19:05:22 -0500

    Paginar resultados y enviar payload

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Main');
+
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(envs.PORT);
   logger.log(`Client Gateway is running on port ${envs.PORT}`);
 }
