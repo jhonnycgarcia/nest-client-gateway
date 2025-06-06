7952f81ab669889650afd8b4990a820a58d2fa74
Jhonny <jhonnycgarcia@gmail.com>
2025-06-05 22:01:50 -0500

    Conectar gateway con orders microservice

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,12 +5,16 @@ interface EnvConfig {
     PORT: number;
     PRODUCTS_MS_HOST: string;
     PRODUCTS_MS_PORT: number;
+    ORDERS_MS_HOST: string;
+    ORDERS_MS_PORT: number;
 }
 
 const envSchema = joi.object({
     PORT: joi.number().required(),
     PRODUCTS_MS_HOST: joi.string().required(),
     PRODUCTS_MS_PORT: joi.number().required(),
+    ORDERS_MS_HOST: joi.string().required(),
+    ORDERS_MS_PORT: joi.number().required(),
 }).unknown(true);
 
 const { value: envConfig, error } = envSchema.validate(process.env);
@@ -25,4 +29,6 @@ export const envs = {
     PORT: envVars.PORT,
     PRODUCTS_MS_HOST: envVars.PRODUCTS_MS_HOST,
     PRODUCTS_MS_PORT: envVars.PRODUCTS_MS_PORT,
+    ORDERS_MS_HOST: envVars.ORDERS_MS_HOST,
+    ORDERS_MS_PORT: envVars.ORDERS_MS_PORT,
 }
---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Post, Body, Param, Logger, Inject } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { ORDERS_SERVICE } from 'src/config';
+import { catchError } from 'rxjs';
+
+@Controller('orders')
+export class OrdersController {
+
+  private readonly logger = new Logger('OrdersController');
+
+  constructor(
+    @Inject(ORDERS_SERVICE) private readonly ordersClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto)
+      .pipe(
+        catchError((error) => {
+          this.logger.error(error.message);
+          throw new RpcException(error);
+        })
+      );
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersClient.send('findAllOrders', {})
+      .pipe(
+        catchError((error) => {
+          this.logger.error(error.message);
+          throw new RpcException(error);
+        })
+      );
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ordersClient.send('findOneOrder', {id});
+  }
+}
